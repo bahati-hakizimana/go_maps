e13c7589418a53df899c76669d3c35406f4d31af
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-04 04:12:46 -0700

    Maps logic

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Maping course")
+
+
+	langwages := make(map[string]string)
+
+	langwages["JS"] = "Javascript"
+	langwages["RB"] = "Ruby"
+	langwages["PY"] = "Python"
+	// langwages["JS"] = "Javascript"
+
+	fmt.Println("List of Langwages", langwages)
+
+
+	delete(langwages,"RB")
+
+	fmt.Println("new list", langwages)
+
+}
